f3d3c3f0192999ea7f5c54df0a8f5d34990e79b3
vvalkov <velizar90@gmail.com>
2014-03-10 14:05:07 +0200

    Second commit. Print-Your-Name problem - working.

--- Print-Your-Name/PrintMyName.cs
+++ Print-Your-Name/PrintMyName.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-    class PrintMyName
+﻿    class PrintMyName
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            System.Console.WriteLine("Velizar Valkov");
         }
     }
