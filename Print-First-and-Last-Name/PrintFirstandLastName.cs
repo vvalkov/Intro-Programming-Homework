5aafce6c2f9feb373304b603f4b828e9e7191e81
vvalkov <velizar90@gmail.com>
2014-03-10 17:56:25 +0200

    Fourth commit. Print-First-and-Last-Name problem - working.

---
+++ Print-First-and-Last-Name/PrintFirstandLastName.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    class PrintFirstandLastName
+    {
+        static void Main()
+        {
+            Console.WriteLine("Velizar");
+            Console.WriteLine("Valkov");
+        }
+    }
