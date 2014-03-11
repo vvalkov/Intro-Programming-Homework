97b493c8769211c27671ff30e4cc9c5f230b132b
vvalkov <velizar90@gmail.com>
2014-03-11 19:12:27 +0200

    Sixth commit. Square-Root problem - working.

---
+++ Square-Root/SquareRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    class SquareRoot
+    {
+        static void Main()
+        {
+            ushort value = 12345;
+            Console.WriteLine(Math.Sqrt(value));
+        }
+    }
