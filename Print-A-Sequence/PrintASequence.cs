357768979cd8371a42e5c22bc67aa464cc6cf722
vvalkov <velizar90@gmail.com>
2014-03-12 22:44:45 +0200

    Eighth commit. Print-A-Sequence - copied.

---
+++ Print-A-Sequence/PrintASequence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class PrintASequence
+{
+    static void Main()
+    {
+        for (int i = 2; i < 13; ++i)
+        {
+            if (i % 2 == 0)
+            {
+                Console.Write(i); Console.Write(",");
+            }
+            else
+            {
+                Console.Write(i * -1); Console.Write(",");
+            }
+        }
+    }
+}
