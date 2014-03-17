26ebe9a9edd485eec0e85c27972ec4512f2c39c6
vvalkov <velizar90@gmail.com>
2014-03-17 17:55:15 +0200

    AgeAfter10Years problem partially done.

---
+++ AgeAfter10Years/AgeAfter10Years.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class AgeAfter10Years
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter your Birth Date in format (dd.mm.yyyy):");
+        DateTime birthday = DateTime.Parse(Console.ReadLine());
+        long age = DateTime.Now.Subtract(birthday).Ticks;
+        if (birthday.Month <= DateTime.Today.Month && birthday.Date <= DateTime.Today.Date) ^ ((birthday.Month < DateTime.Today.Month && birthday.Date > DateTime.Today.Date));
+        {
+            Console.WriteLine("You are {0} years old", new DateTime(age).Year);
+            Console.WriteLine("After 10 years you will be {0} years old", new DateTime(age).AddYears(10).Year);
+        }
+        else
+        {
+            Console.WriteLine("You are {0} years old", new DateTime(age).Year - 1);
+            Console.WriteLine("After 10 years you will be {0} years old", new DateTime(age).AddYears(9).Year);
+        }
+    }
+}
+          
