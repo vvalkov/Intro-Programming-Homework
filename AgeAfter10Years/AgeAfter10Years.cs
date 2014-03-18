29c9324b602e93a1146402a1fb0092cd3c3ec062
vvalkov <velizar90@gmail.com>
2014-03-18 18:42:59 +0200

    New solution to AgeAfter10Years.cs partially working.

--- AgeAfter10Years/AgeAfter10Years.cs
+++ AgeAfter10Years/AgeAfter10Years.cs
@@ -7,7 +7,9 @@ class AgeAfter10Years
         Console.WriteLine("Please enter your Birth Date in format (dd.mm.yyyy):");
         DateTime birthday = DateTime.Parse(Console.ReadLine());
         long age = DateTime.Now.Subtract(birthday).Ticks;
-        if (birthday.Month <= DateTime.Today.Month && birthday.Date <= DateTime.Today.Date) ^ ((birthday.Month < DateTime.Today.Month && birthday.Date > DateTime.Today.Date));
+        if ((DateTime.Now.Month > birthday.Month || 
+            (DateTime.Now.Month == birthday.Month) && 
+            DateTime.Now.Day > birthday.Day))
         {
             Console.WriteLine("You are {0} years old", new DateTime(age).Year);
             Console.WriteLine("After 10 years you will be {0} years old", new DateTime(age).AddYears(10).Year);
@@ -19,4 +21,18 @@ class AgeAfter10Years
         }
     }
 }
-          
+
+//using System;
+
+//class AgeAfter10Years
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Please enter your Birth Date in format (dd.mm.yyyy):");
+//        DateTime birthday = DateTime.Parse(Console.ReadLine());
+//        TimeSpan age = DateTime.Now - birthday;
+//        TimeSpan after10years = DateTime.Now.AddYears(10) - DateTime.Now;
+//        Console.WriteLine("You are {0:MM.dd/yy} years old", age.TotalDays / 365.2);
+//        Console.WriteLine("After 10 years you will be {0:} years old", age.Add(after10years));
+//    }
+//}
