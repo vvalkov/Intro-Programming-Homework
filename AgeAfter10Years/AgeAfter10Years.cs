f2b9e05466451a5921677b061c8160470aae144c
vvalkov <velizar90@gmail.com>
2014-03-21 17:19:35 +0200

    AgeAfter10Years working.

--- AgeAfter10Years/AgeAfter10Years.cs
+++ AgeAfter10Years/AgeAfter10Years.cs
@@ -4,35 +4,11 @@ class AgeAfter10Years
 {
     static void Main()
     {
-        Console.WriteLine("Please enter your Birth Date in format (dd.mm.yyyy):");
+        Console.WriteLine("Please enter your Birth Date in format (dd.mm.yyyy):"); //Please make sure your regional settings are according to the format
         DateTime birthday = DateTime.Parse(Console.ReadLine());
-        long age = DateTime.Now.Subtract(birthday).Ticks;
-        if ((DateTime.Now.Month > birthday.Month || 
-            (DateTime.Now.Month == birthday.Month) && 
-            DateTime.Now.Day > birthday.Day))
-        {
-            Console.WriteLine("You are {0} years old", new DateTime(age).Year);
-            Console.WriteLine("After 10 years you will be {0} years old", new DateTime(age).AddYears(10).Year);
-        }
-        else
-        {
-            Console.WriteLine("You are {0} years old", new DateTime(age).Year - 1);
-            Console.WriteLine("After 10 years you will be {0} years old", new DateTime(age).AddYears(9).Year);
-        }
+        TimeSpan age = DateTime.Now - birthday;
+        TimeSpan after10years = age.Add(DateTime.Now.AddYears(10) - DateTime.Now);
+        Console.WriteLine("You are {0} years old", Math.Truncate(age.TotalDays /365.25));
+        Console.WriteLine("After 10 years you will be {0} years old", Math.Truncate(after10years.TotalDays /365.25));
     }
 }
-
-//using System;
-
-//class AgeAfter10Years
-//{
-//    static void Main()
-//    {
-//        Console.WriteLine("Please enter your Birth Date in format (dd.mm.yyyy):");
-//        DateTime birthday = DateTime.Parse(Console.ReadLine());
-//        TimeSpan age = DateTime.Now - birthday;
-//        TimeSpan after10years = DateTime.Now.AddYears(10) - DateTime.Now;
-//        Console.WriteLine("You are {0:MM.dd/yy} years old", age.TotalDays / 365.2);
-//        Console.WriteLine("After 10 years you will be {0:} years old", age.Add(after10years));
-//    }
-//}
